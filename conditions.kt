2d69db4977c1f53d066936ab3dc75ddb0a198123
xxRMxx <raphae7.meier@gmail.com>
2020-11-06 22:01:25 +0000

    Introduction into Kotlin
    Signed-off-by: xxRMxx <raphae7.meier@gmail.com>

---
+++ conditions.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    val num1 = readLine()!!
+    val num2 = readLine()!!
+
+    println("$num1")
+    println("$num2")
+
+    /*
+    if num1.compareTo(num2)
+    {
+        println("num1 bigger than num2")
+    } else if (num2 > num1)
+    {
+        println("num2 bigger than num1")
+    } else
+    {
+        println("num1 equals to num2")
+    }
+    */
+}
---
+++ variables.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    // declaring some int variables and counting the sum
+    var num1 = 1 // automatically assigned as int
+
+    var num2: Int // explicitely assigned as int
+    num2 = 2
+
+    var sum = num1 + num2
+
+    // declaring some string variables and join some to one text
+    var text1 = "Hello"
+    var text2 = "world"
+    var text = text1 + " " + text2
+
+    // just some output
+    println("sum: " + sum)
+    println("text: " + text)
+}
